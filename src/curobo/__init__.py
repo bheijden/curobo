f7cff19c6a491381a93fe7c7b1d9681cc4f0f3d1
Bas van der Heijden <b.heijden@hotmail.com>
2025-11-05 17:30:58 +0100

    Make metadata fully static by removing setuptools_scm
    Remove setuptools_scm dependency and make version static to enable
    full static metadata resolution. This allows uv and other tools to
    resolve dependencies without executing any code.
    
    Changes:
    - Remove setuptools_scm from build-system.requires
    - Set static version = "0.8.0" instead of dynamic version
    - Remove setuptools_scm from runtime dependencies
    - Remove [tool.setuptools_scm] configuration section
    - Simplify __init__.py to use importlib.metadata for version
    
    With fully static metadata, uv can now use match-runtime for torch
    in downstream projects without needing dependency-metadata overrides.

--- src/curobo/__init__.py
+++ src/curobo/__init__.py
@@ -44,41 +44,8 @@ cuRobo package is split into several modules:
 
 # NOTE (roflaherty): This is inspired by how matplotlib does creates its version value.
 # https://github.com/matplotlib/matplotlib/blob/master/lib/matplotlib/__init__.py#L161
-def _get_version():
-    """Return the version string used for __version__."""
-    # Standard Library
-    import pathlib
-
-    root = pathlib.Path(__file__).resolve().parent.parent.parent
-    if (root / ".git").exists() and not (root / ".git/shallow").exists():
-        # Third Party
-        import setuptools_scm
-
-        # See the `setuptools_scm` documentation for the description of the schemes used below.
-        # https://pypi.org/project/setuptools-scm/
-        # NOTE: If these values are updated, they need to be also updated in `pyproject.toml`.
-        return setuptools_scm.get_version(
-            root=root,
-            version_scheme="no-guess-dev",
-            local_scheme="dirty-tag",
-        )
-    else:  # Get the version from the _version.py setuptools_scm file.
-        try:
-            # Standard Library
-            from importlib.metadata import version
-        except ModuleNotFoundError:
-            # NOTE: `importlib.resources` is part of the standard library in Python 3.9.
-            # `importlib_metadata` is the back ported library for older versions of python.
-            # Third Party
-            from importlib_metadata import version
-        try:
-            return version("nvidia_curobo")
-        except:
-            return "v0.7.0-no-tag"
-
-
-# Set `__version__` attribute
-__version__ = _get_version()
-
-# Remove `_get_version` so it is not added as an attribute
-del _get_version
+try:
+    from importlib.metadata import version as _v
+except ModuleNotFoundError:
+    from importlib_metadata import version as _v
+__version__ = _v("nvidia_curobo")
